b7b8b759834e745c13466d1b26c67c42d26775d0
unknown <vishnutoanother@gmail.com>
2023-05-02 15:24:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useContext } from 'react';
+import {Routes, Route} from 'react-router-dom';
+import "./App.css"
+
+import BlogPost from './component/BlogPost';
+import Form from './component/Form';
+import Bloglist from './component/Bloglist';
+import { UserContext } from './component/context';
+import {Link} from "react-router-dom"
 
 function App() {
+  const [items, setItems] = useState([]);
+  const state={
+    text:items,
+    settext:setItems
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className='heading'><Link to="/" >Home</Link> &nbsp;
+      <Link to="/bloglist" >Bloglist</Link> </h2>
+    
+   
+     
+ 
+     <UserContext.Provider value={state}>
+        <Routes>
+          {/* <Switch>  */}
+          <Route path="/" element={<Form />} />
+          
+          <Route path="/bloglist" element={<Bloglist  />} />  
+          <Route path='/blogpost/:userid' element={<BlogPost />} />
+  
+        
+          {/* </Switch> */}
+                  </Routes>
+                
+        </UserContext.Provider>
+    
+   
     </div>
   );
 }
 
-export default App;
+export default App; 
